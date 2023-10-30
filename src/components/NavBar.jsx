c46bcedc4733de0f1065021f5f7417a45aee6e52
parnia mohammadi <parniiiiia.mohammadi@gmail.com>
2023-10-30 17:49:03 +0330

    useEffect with changing state

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,41 @@
 import { HeartIcon , SparklesIcon } from "@heroicons/react/24/outline";
-function NavBar({characters}) {
+function NavBar({children}) {
   return (
     <nav className="navbar">
-      <div className="navbar__logo"><span>Logo </span>
-        <SparklesIcon style={{width: "1.5rem" ,height: "1.5rem", color:"yellow"
-        }}/>
-      </div>
-      <input type="text" placeholder="search ..." className="text-field" />
-      <div className="navbar__result">Found {characters.length} character</div>
-      <button className="heart">
-        <HeartIcon className="icon"/>
-        <span className="badge">4</span>
-      </button>
+      <NavbarLogo/>
+      {children}
+      <Faivorites/>
     </nav>
   );
 }
 
 export default NavBar;
+
+function NavbarLogo(){
+  return(
+    <div className="navbar__logo"><span>Logo </span>
+    <SparklesIcon style={{width: "1.5rem" ,height: "1.5rem", color:"yellow"
+    }}/>
+  </div>
+  );
+}
+
+function Faivorites(){
+  return(
+    <button className="heart">
+      <HeartIcon className="icon"/>
+      <span className="badge">4</span>
+    </button>
+  );
+}
+
+export function Search({search, setSearch}){
+  return(
+  <input value={search} type="text" placeholder="search ..." 
+  className="text-field" onChange={(e) => setSearch(e.target.value)} />);
+}
+export function SearchResult({searchResult}){
+  return(
+    <div className="navbar__result">Found {searchResult} character</div>
+  );
+}
